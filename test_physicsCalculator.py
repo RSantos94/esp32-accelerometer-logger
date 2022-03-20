6879ff928049a16d41ce33694c56cd9236bbb10b
Rúben Santos <rubenfjs.94@gmail.com>
2022-03-20 22:52:00 +0000

    Remove stuff related to impact detection

--- test_physicsCalculator.py
+++
@@ -1,9 +0,0 @@
-from physicsCalculator import PhysicsCalculator
-
-
-def test_calculate_acceleration():
-    assert PhysicsCalculator.calculate_acceleration(10, )
-
-
-def test_calculate_force():
-    assert PhysicsCalculator.calculate_force(10, 5) == 50
